9ec0ee2fb4674928b068b5bac80f5ad4a104c68b
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2021-03-02 06:28:11 -0700

    alloc array & fill

--- project2.c
+++ project2.c
@@ -5,5 +5,20 @@
 #include <stdbool.h>
 
 int main(int argc, char* argv[]) {
+	// command line syntax is "project2 SIZE THRESHOLD [SEED [MULTITHREAD [PIECES [THREADS]]]]"
 	printf("Found %d args\n", argc);
+	if (argc < 3) {
+		printf("Not enough args.");
+		return 1;
+	}
+	int size = atoi(argv[2]);
+
+	int* array = (int*) malloc(sizeof(int)*size);
+
+	for (int i = 0; i < size; i++) {
+		// Fill our dynamic array
+		array[i] = i;
+	}
+
+	return 0;
 }

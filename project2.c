4b5095aa2f7e3a7e33d81918ba18c42221d35422
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2021-03-05 14:02:08 -0700

    hella comments

--- project2.c
+++ project2.c
@@ -1,3 +1,20 @@
+/*
+	File: project2.c - Implements a hybrid sorting process in C
+
+	This file implements a hybrid sort, that uses both the quickSort
+	and shellSort algorithms, as well as posix style pthreads
+	to quickly and effectively sort large arrays multithreaded.
+	Takes in between 2 and 6 arguments, specifying, in order, the size of the array,
+	when to switch to shellsort, the seed for the random number generator,
+	whether or not to multithread, the number of pieces to partition the
+	array into before sorting, and the maximum number of threads to spin up concurrently.
+
+	Author: Quinn Kleinfelter
+	Class: EECS 3540-001 Operating Systems & Systems Programming Spring 2021
+	Instructor: Dr. Thomas
+	Last Edited: 3/5/21
+	Copyright: Copyright 2021 by Quinn Kleinfelter. All rights reserved.
+*/
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,10 +28,10 @@
 // Global variables
 int* array;
 int arraySize;
-int threshold;
-bool shouldMultithread = true;
-int numPartitions = 10;
-int maxThreads = 4;
+int threshold; // when we switch from quicksort to shellsort
+bool shouldMultithread = true; 
+int numPartitions = 10; // number of pieces to split the array into
+int maxThreads = 4; // max threads to run at once
 
 // Methods
 bool isSorted();
@@ -31,11 +48,11 @@ typedef struct {
 } range;
 
 int main(int argc, char* argv[]) {
+	// Clock values for the beginning of everything
 	clock_t veryStart = clock();
 	struct timeval veryStartWall;
 	gettimeofday(&veryStartWall, NULL);
 
-	// command line syntax is "project2 SIZE THRESHOLD [SEED [MULTITHREAD [PIECES [THREADS]]]]"
 	if (argc < 3) {
 		printf("Correct usage: project2 SIZE THRESHOLD [SEED [MULTITHREAD [PIECES [THREADS]]]]\n");
 		return 1;
@@ -44,7 +61,6 @@ int main(int argc, char* argv[]) {
 	threshold = atoi(argv[2]);
 
 	if (argc >= 4) {
-		printf("Seed specified\n");
 		// At least 4 args, must have a seed
 		if (atoi(argv[3]) == -1) srand(clock());
 		else srand(atoi(argv[3]));
@@ -53,20 +69,16 @@ int main(int argc, char* argv[]) {
 	if (argc >= 5) {
 		// At least 5 args, must have a multithread choice
 		if (strncmp(argv[4], "n", 1) == 0|| strncmp(argv[4], "N", 1) == 0) {
-			printf("Not multithreading\n");
 			shouldMultithread = false;
 		}
-		else printf("Multithreading\n");
 	}
 
 	if (argc >= 6) {
 		// At least 6 args, must specify number of pieces
-		// TODO: Error Handling
 		if (shouldMultithread) {
 			int partsSpec = atoi(argv[5]);
 			if (partsSpec > 0) numPartitions = partsSpec;
-			printf("Number of partitions: %d\n", numPartitions);
-		}
+		} // if we aren't multithreading, then it doesn't matter whats specified
 	}
 
 	if (argc >= 7) {
@@ -74,20 +86,22 @@ int main(int argc, char* argv[]) {
 		if (shouldMultithread) {
 			int threadsSpec = atoi(argv[6]);
 			if (threadsSpec > numPartitions) {
+				// We can't have more threads than pieces
 				printf("Too many threads specified\n");
 				return 1;
 			} else {
 				maxThreads = threadsSpec;
-				printf("Max threads: %d\n", maxThreads);
 			}
-		}
+		} // again, requires multithreading
 	}
 
+	// Creating the array & timing it
 	clock_t start = clock();
 	array = (int*) malloc(sizeof(int)*arraySize);
 	clock_t end = clock();
 	printf("Created array in %3.3f seconds\n", (double) (end - start) / 1000000);
 
+	// Initializing the array & timing it
 	start = clock();
 	for (int i = 0; i < arraySize; i++) {
 		// Fill our dynamic array
@@ -96,8 +110,8 @@ int main(int argc, char* argv[]) {
 	end = clock();
 	printf("Initialized array in %3.3f seconds\n", (double) (end - start) / 1000000);
 
+	// Randomizing the array & timing it
 	start = clock();
-	// Randomize array
 	for (int i = 0; i < arraySize; i++) {
 		int secondIndex = rand() % arraySize;
 		swap(&array[i], &array[secondIndex]);
@@ -105,19 +119,25 @@ int main(int argc, char* argv[]) {
 	end = clock();
 	printf("Randomized array in %3.3f seconds\n", (double) (end - start) / 1000000);
 
-	bool sorted = isSorted();
-	// Shouldn't be sorted
-	if (sorted) {
-		printf("Array is sorted!\n");
-	} else {
-		printf("Array is not sorted! :(\n");
-	}
+	// bool sorted = isSorted();
+	// // Shouldn't be sorted
+	// if (sorted) {
+	// 	printf("Array is sorted!\n");
+	// } else {
+	// 	printf("Array is not sorted! :(\n");
+	// }
 
+	// timing structure, used either in multithreaded mode or not, so keep it out of the if
 	struct timeval startTime;
 	if (shouldMultithread) {
+		// If we're multithreading, the first thing we need to do is partition
+		// the array into numPartitions pieces
+		
+		// Start timing how long it takes to partition
 		start = clock();
+		// we start currPieces at 1 because we're going to make the first piece before the loop
 		int currPieces = 1;
-		// Array of ranges which are the partitions we have
+		// Array of ranges which are the partitions we have currently
 		range* pieces = (range*) malloc(numPartitions * sizeof(range));
 
 		// Our first "piece" is the whole array
@@ -125,35 +145,58 @@ int main(int argc, char* argv[]) {
 		pieces[0].R = arraySize - 1;
 
 		while (currPieces < numPartitions) {
+			// While we have less pieces than the number of partitions specified
+			// we loop and partition continuously
+
+			// Finding the largest partition, start with the first one
 			range* largest = &pieces[0];
 			int largestSize = largest->R - largest->L + 1;
 
+			// Loop through the current pieces we have
 			for (int i = 1; i < currPieces; i++) {
 				range* current = &pieces[i];
 				int currSize = current->R - current->L + 1;
+
+				// if the size of the current piece is greater than the largest
+				// then we need to make this one the largest
 				if (currSize > largestSize) {
 					largest = current;
 					largestSize = currSize;
 				}
 			}
 
+			// debug sizing information
 			//printf("Partitioning %d - %d (%d)...result: ", largest->L, largest->R, largestSize);
 
+			// Now that we've determined our largest piece, we can partition it into 
+			// 2 smaller pieces, with the pivot value being stored in midpt
 			int midpt = partition(largest->L, largest->R);
 
+			// the new partition always goes on the spot 1 after our last one in the list
+			// which is located at currPieces because that is 1-indexed while the array is 0-indexed
 			range* newPart = &pieces[currPieces];
-			newPart->L = midpt + 1;
+
+			// this new piece needs to be the right half of the partition so we start it at the midpt + 1
+			// and it goes until the largest's right half
+			newPart->L = midpt + 1; 
 			newPart->R = largest->R;
 
+			// then, we change the largest's right half value to be the midpt - 1
+			// so that it becomes the left half of the partition
 			largest->R = midpt - 1;
-			int ls = largest->R - largest->L + 1;
-			int ns = newPart->R - newPart->L + 1;
-			int ts = ls + ns;
+
+			// debug sizing information
+			// int ls = largest->R - largest->L + 1;
+			// int ns = newPart->R - newPart->L + 1;
+			// int ts = ls + ns;
 			//printf("%d - %d (%2.2f / %2.2f)\n", ls, ns, (double) ls / ts, (double) ns / ts);
 
+			// finally, we can increment currPieces and continue looping
 			currPieces++;
 		}
+		// Once we're done partitioning we can finish the timing and print it
 		end = clock();
+		printf("Partitioned array in %3.3f seconds\n", (double) (end - start) / 1000000);
 
 		// Now we have the list of partitions, so we should sort these in descending
 		// order by size, so we can shoot them off into threads largest -> smallest
@@ -169,8 +212,8 @@ int main(int argc, char* argv[]) {
 			pieces[j + 1] = current;
 		}
 
-		printf("Partitioned array in %3.3f seconds\n", (double) (end - start) / 1000000);
-
+		// Now that we have our list of partitions sorted appropriately we can start the timing for
+		// the multithreading and get to work spawning out multiple threads
 		start = clock();
 		gettimeofday(&startTime, NULL);
 
@@ -178,10 +221,12 @@ int main(int argc, char* argv[]) {
 		// Array of threads & thread attributes
 		pthread_t* threads = (pthread_t*) malloc(maxThreads * sizeof(pthread_t));
 		pthread_attr_t* threadAttributes = (pthread_attr_t*) malloc(maxThreads * sizeof(pthread_attr_t));
+		// variable to keep track of the next thread to shoot off
 		int nextThread = 0;
 
 		for (int i = 0; i < maxThreads; i++) {
 			// Spawn off our initial maxThreads threads
+
 			// grab the current piece
 			range* piece = &pieces[nextThread];
 			// setup its attributes
@@ -196,12 +241,13 @@ int main(int argc, char* argv[]) {
 		while (nextThread < numPartitions) {
 			while (true) {
 				for (int i = 0; i < maxThreads; i++) {
+					// grab the thread at our current index
 					pthread_t* currThread = &threads[i];
 
 					// Check if the current thread has been completed
 					// and if it has, spawn another one
 					if (pthread_tryjoin_np(*currThread, NULL) == 0) {
-						// grab the current piece
+						// grab the next one we need to spawn
 						range* piece = &pieces[nextThread];
 						// setup its attributes
 						pthread_attr_init(&threadAttributes[i]);
@@ -210,12 +256,21 @@ int main(int argc, char* argv[]) {
 						goto next; // get out of the inner loops and go again
 					}
 				}
+				// Don't poll the threads again immediately, because its unlikely they've finished
+				// instead wait 50ms
 				usleep(50000);
 			}
+			// increment the thread tracker and go back into the infinite loop
+			// OR we'll hit the nextThread >= numPartitions condition, and break out
+			// because we'll have spawned all the threads needed to finish the sort
 			next: nextThread++;
 		}
 
+		// Now we can loop through our remaining running threads
+		// and wait for them to terminate to join them up
 		for (int i = 0; i < maxThreads; i++) {
+			// join waits for the thread to terminate before returning
+			// so we can just loop through them all once
 			pthread_join(threads[i], NULL);
 		}
 
@@ -223,25 +278,28 @@ int main(int argc, char* argv[]) {
 		free(threadAttributes);
 		free(threads);
 	} else {
-		// No multithreading, so we can just sort
+		// No multithreading, so we can just sort & time it
 		start = clock();
 		gettimeofday(&startTime, NULL);
 		quickSort(0, arraySize - 1);
 	}
 
+	// At this point the array is sorted, so we can finish up the timing and print the results
 	end = clock();
 	struct timeval endTime;
 	gettimeofday(&endTime, NULL);
 	// TODO: wall clock time is broken
 	printf("Seconds spent sorting: Wall Clock: %3.3f / CPU: %3.3f\n", (double) endTime.tv_sec - startTime.tv_sec, (double) (end - start) / 1000000);
 
+	// as well as finish up the timing for the overall program
 	clock_t veryEnd = clock();
 	struct timeval veryEndWall;
 	gettimeofday(&veryEndWall, NULL);
 	// TODO: wall clock time is broken
 	printf("Seconds spent overall: Wall Clock: %3.3f / CPU: %3.3f\n", (double) veryEndWall.tv_sec - veryStartWall.tv_sec, (double) (veryEnd - veryStart) / 1000000);
 
-	sorted = isSorted();
+	// Make sure the array is actually sorted before we finish the program!!!
+	bool sorted = isSorted();
 	// Should be sorted
 	if (sorted) {
 		printf("Array is sorted!\n");
@@ -249,6 +307,7 @@ int main(int argc, char* argv[]) {
 		printf("Array is not sorted! :(\n");
 	}
 
+	// free up the array and return success
 	free(array);
 	return 0;
 }
@@ -257,12 +316,16 @@ bool isSorted() {
 	// Checks if the global array variable is sorted
 	for (int i = 0; i < arraySize; i++) {
 		if (array[i] != i) {
+			// if the array position isn't equivalent to its number
+			// we know it isn't sorted since our array is from 0 to arraySize - 1
 			return false;
 		}
 	}
+	// if we make it here, then we must have a sorted array
 	return true;
 }
 
+// helper function that swaps 2 int variables
 void swap(int* a, int* b) {
 	int temp = *a;
 	*a = *b;
@@ -272,15 +335,18 @@ void swap(int* a, int* b) {
 void quickSort(int p, int r) {
 	if (p < r) { // base case: when down to 1 item, p == r
 		int size = r - p + 1;
-		if (size < 2) return;
+		if (size < 2) return; // nothing to sort
 		if (size == 2) {
+			// check if we need to swap our 2 elements
 			if (array[p] > array[r]) {
 				swap(&array[p], &array[r]);
 			}
 		} else if (threshold != 0 && r - p + 1 <= threshold) {
 			// Shellsort instead of quicksort
+			// if our remaining number of elements is less than the threshold
 			shellSort(p, r);			
 		} else {
+			// otherwise continue quicksorting
 			int q = partition(p, r); // split the list, pivot is q
 			quickSort(p, q - 1); // recursively quicksort the left side
 			quickSort(q + 1, r); // recursively quicksort the right side
@@ -289,23 +355,33 @@ void quickSort(int p, int r) {
 }
 
 int partition(int p, int r) {
+	// start i at the left side
 	int i = p;
+	// and j one past the right side
 	int j = r + 1;
+	// with x being the value on the left
 	int x = array[p];
 	do {
+		// increase i over and over again while its value is less than the leftmost value
 		do i++; while (array[i] < x);
+		// decrease j over and over again while its value is greater than the leftmost value
 		do j--; while (array[j] > x);
 		if (i < j) {
+			// if i < j we need to swap the values at i and j
 			swap(&array[i], &array[j]);
 		} else {
+			// otherwise break out
 			break;
 		}
 	} while (true);
+	// then swap the values at p and j
 	swap(&array[p], &array[j]);
+	// and return j, our pivot value
 	return j;
 }
 
 void shellSort(int low, int hi) {
+	// hibbards sequence
 	int k = 1;
 	int size = hi - low + 1;
 	while (k <= size) {
@@ -314,9 +390,9 @@ void shellSort(int low, int hi) {
 	k = (k / 2) - 1;
 
 	do {
-		for (int i = low; i < (low + size - k); i++) {
-			for (int j = i; j >= low; j -= k) {
-				if (array[j] <= array[j + k]) break;
+		for (int i = low; i < (low + size - k); i++) { // for each comb position
+			for (int j = i; j >= low; j -= k) { // tooth to tooth is k
+				if (array[j] <= array[j + k]) break; // move upstream/exit?
 				else swap(&array[j], &array[j+k]);
 			}
 		}
@@ -325,8 +401,13 @@ void shellSort(int low, int hi) {
 }
 
 void* runner(void* parameters) {
+	// runner for our multithreading
+	// grab the range specified in the parameters
 	range* params = (range*) parameters;
+	// Print the left side, right side, and size as we start the thread
 	printf("(%d, %d, %d)\n", params->L, params->R, params->R - params->L + 1);
+	// quicksort on the thread from the left to the right
 	quickSort(params->L, params->R);
+	// and exit the thread once we're here
 	pthread_exit(0);
 }
